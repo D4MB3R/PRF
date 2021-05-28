8d26f9b7d614c0dfdb27add3fe47336813078777
Adam Berec <48825185+D4MB3R@users.noreply.github.com>
2021-05-28 22:06:33 +0200

    Update index.js

--- backend_nodejs/index.js
+++ backend_nodejs/index.js
@@ -7,7 +7,7 @@ const mongoose = require('mongoose');
 const app = express();
 
 const port = process.env.PORT || 3000;
-const dbUrl = 'mongodb+srv://admin:admin@prfcluster.eagwk.mongodb.net/test';
+const dbUrl = 'mongodb+srv://***:***@prfcluster.eagwk.mongodb.net/test';
 
 mongoose.connect(dbUrl);
 
